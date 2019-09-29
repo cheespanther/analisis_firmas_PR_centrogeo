6f37837bbc1ce0ef923366d807c80de271a561b1
cheesepanther <iskarwaluyo@gmail.com>
2019-09-28 21:30:56 -0500

    Update analisisfirmas_PR.R

--- analisisfirmas_PR.R
+++ analisisfirmas_PR.R
@@ -1,9 +1,9 @@
-## FUNCION PARA LEER LOS ARCHIVOS DE LAS FIRMAS DEL ELECTRORADIOESPECTROMETRO CENTRO GEO ESPECIALIDAD GRUPO "B" 2019
-
 library(curl)
 library(RCurl)
 library(rio)
 library(dygraphs)
+library(rlist)
+
 
 leer_firmas <<- function(id, summarize = TRUE) 
 {
@@ -11,14 +11,27 @@ leer_firmas <<- function(id, summarize = TRUE)
   ## CREA NOMBRE DE ARCHIVOS CON 5 DIGITOS (0´S A LA IZQUIERDA) SEGUN EL "id"
   filename <<- paste("https://raw.githubusercontent.com/cheespanther/analisis_firmas_PR_centrogeo/master/datos/ref",sprintf("%05d", id), ".asd",".txt", sep="")
   ## LEE ARCHIVOS SEGUN EL "id" Y EL DIRECTORIO DONDE SE ENCUENTREN
-  datos <<- lapply(paste(filename), read.delim, header=TRUE) 
+  datos <<- lapply(paste(filename), read.delim, header=TRUE)
+  
+  datos <<- datos$
+  
+  datos_tabla <- list.cbind(datos)
+  
+  columna_seleccion <<- paste("ref",sprintf("%05d", id), ".asd", sep="")
 
   return (datos)
 }
 
-p <- dygraph(datos_tabla) %>%
-  dyOptions(labelsUTC = TRUE, fillGraph=TRUE, fillAlpha=0.1, drawGrid = FALSE, colors="#D8AE5A") %>%
+dt <- datos_tabla[ , columna_seleccion]
+longitud <- datos$Wavelength
+dt <- cbind(longitud, dt)
+
+
+p <- dygraph(dt) %>%
+  dyOptions(labelsUTC = TRUE, fillGraph=TRUE, fillAlpha=0.1, drawGrid = TRUE) %>%
   dyRangeSelector() %>%
   dyCrosshair(direction = "vertical") %>%
   dyHighlight(highlightCircleSize = 5, highlightSeriesBackgroundAlpha = 0.2, hideOnMouseOut = FALSE)  %>%
-  dyRoller(rollPeriod = 1)
+  dyRoller(rollPeriod = 1) %>%
+  dyAxis("x", label = "Longitud de onda") %>%
+  dyAxis("y", label = "Valor")
