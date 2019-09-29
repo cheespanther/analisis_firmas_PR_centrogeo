7b67fef7b2826d0f57091b42b8ad527c9c2fd130
cheesepanther <iskarwaluyo@gmail.com>
2019-09-29 10:57:49 -0500

    Update analisisfirmas_PR.R

--- analisisfirmas_PR.R
+++ analisisfirmas_PR.R
@@ -4,36 +4,33 @@ library(rio)
 library(dygraphs)
 library(rlist)
 
-leer_firmas <<- function(id, summarize = TRUE) 
+
+graficar_firmas <<- function(id, summarize = TRUE) 
 {
   ## CREAR LISTA DE ARCHIVOS PARA LEER
   ## CREA NOMBRE DE ARCHIVOS CON 5 DIGITOS (0´S A LA IZQUIERDA) SEGUN EL "id"
-  filename <<- paste("https://raw.githubusercontent.com/cheespanther/analisis_firmas_PR_centrogeo/master/datos/ref",sprintf("%05d", id), ".asd",".txt", sep="")
+  filename <- paste("https://raw.githubusercontent.com/cheespanther/analisis_firmas_PR_centrogeo/master/datos/ref",sprintf("%05d", id), ".asd",".txt", sep="")
   ## LEE ARCHIVOS SEGUN EL "id" Y EL DIRECTORIO DONDE SE ENCUENTREN
-  datos <<- lapply(paste(filename), read.delim, header=TRUE)
+  datos <- lapply(paste(filename), read.delim, header=TRUE)
   
-  # JUNTAR LOS DATOS SELECCIONADOS EN UNA TABLA
-  datos_tabla <<- list.cbind(datos)
-  longitud <<- datos_tabla$Wavelength
+  datos_tabla <- list.cbind(datos)
+  longitud <- datos_tabla$Wavelength
   
-  # HACER LISTA DE COLUMNAS DE LOS DATOS
-  columna_seleccion <<- paste("ref",sprintf("%05d", id), ".asd", sep="")
+  columna_seleccion <- paste("ref",sprintf("%05d", id), ".asd", sep="")
   
-  # SELECCIONAR LAS COLUMNAS CON LOS DATOS PARA NO DUPLICAR LAS LONGITUDES DE ONDA
+  dt <- datos_tabla[ , columna_seleccion]
+  dt <- as.data.frame(cbind(longitud, dt))
   
-  # JUNTAR TABLA DE DATOS CON COLUMNA DE LONGITUDES DE ONDA
-  dt <<- datos_tabla[ , columna_seleccion]
-  dt <<- as.data.frame(cbind(longitud, dt))
+  print.data.frame(dt)
 
-  return (dt)
+  p <- dygraph(dt) %>%
+    dyOptions(labelsUTC = TRUE, fillGraph=FALSE, fillAlpha=0.1, drawGrid = TRUE) %>%
+    dyRangeSelector() %>%
+    dyCrosshair(direction = "vertical") %>%
+    dyHighlight(highlightCircleSize = 5, highlightSeriesBackgroundAlpha = 0.2, hideOnMouseOut = FALSE)  %>%
+    dyRoller(rollPeriod = 1) %>%
+    dyAxis("x", label = "Longitud de onda") %>%
+    dyAxis("y", label = "Valor")
+  
+  return(p)
 }
-
-# GRAFICAR CON DYGRAPH
-p <- dygraph(dt) %>%
-  dyOptions(labelsUTC = TRUE, fillGraph=TRUE, fillAlpha=0.1, drawGrid = TRUE) %>%
-  dyRangeSelector() %>%
-  dyCrosshair(direction = "vertical") %>%
-  dyHighlight(highlightCircleSize = 5, highlightSeriesBackgroundAlpha = 0.2, hideOnMouseOut = FALSE)  %>%
-  dyRoller(rollPeriod = 1) %>%
-  dyAxis("x", label = "Longitud de onda") %>%
-  dyAxis("y", label = "Valor")
